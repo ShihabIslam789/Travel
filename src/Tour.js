360290afdafafe068e41e454ab73378fdd5f7d13
Shihab Islam <56773545+ShihabIslam789@users.noreply.github.com>
2023-01-25 22:20:09 -0500

    Update Tour.js
    starter code

---
+++ src/Tour.js
@@ -0,0 +1,7 @@
+import React, { useState } from 'react';
+
+const Tour = ({ id, image, info, name, price, removeTour }) => {
+  const [readMore, setReadMore] = useState(false);
+  return (
+    <article className="single-tour">
+      <img src={image} alt={name} />
