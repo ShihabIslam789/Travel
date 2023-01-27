64a67de8c25b39c77ddb614ae6295b3e6d5b9f7e
Shihab Islam <56773545+ShihabIslam789@users.noreply.github.com>
2023-01-26 21:32:24 -0500

    Update Tour.js
    added the delete button to remove selections.

--- src/Tour.js
+++ src/Tour.js
@@ -16,3 +16,12 @@ const Tour = ({ id, image, info, name, price, removeTour }) => {
             {readMore ? 'show less' : '  read more'}
           </button>
         </p>
+        <button className="delete-btn" onClick={() => removeTour(id)}>
+          not interested
+        </button>
+      </footer>
+    </article>
+  );
+};
+
+export default Tour;
